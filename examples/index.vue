641403ef8946055d619d7d548ff75055cf12df5f
iyanghong <897541333@qq.com>
2021-06-30 10:50:30 +0800

    修改示例入口为examples目录

---
+++ examples/index.vue
@@ -0,0 +1,20 @@
+<template>
+<!--    <topology-editor></topology-editor>-->
+    <markdown-editor></markdown-editor>
+</template>
+
+<script>
+    import {
+        markdownEditor,
+    } from '../src/index'
+
+    export default {
+        name: 'TopologyView',
+        components: {
+            markdownEditor
+        }
+    };
+</script>
+<style lang="scss" scoped>
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by yh on 2021/6/29
+ */
+module.exports = {
+    // 修改 src 为 examples
+    pages: {
+        index: {
+            entry: 'examples/main.js',
+            template: 'public/index.html',
+            filename: 'index.html'
+        }
+    }
+}
